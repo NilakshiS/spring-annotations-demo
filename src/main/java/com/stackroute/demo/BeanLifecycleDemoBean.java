f4738826f8ed25918c661d53fd036050127b8140
Nilakshi <nilakshi.ma1509@ict.gnu.ac.in>
2019-07-15 09:30:41 +0530

    added custom init and destroy using annotation

--- src/main/java/com/stackroute/demo/BeanLifecycleDemoBean.java
+++ src/main/java/com/stackroute/demo/BeanLifecycleDemoBean.java
@@ -3,6 +3,9 @@ package com.stackroute.demo;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class BeanLifecycleDemoBean implements InitializingBean, DisposableBean {
     @Override
     public void destroy() throws Exception {
@@ -14,11 +17,12 @@ public class BeanLifecycleDemoBean implements InitializingBean, DisposableBean {
         System.out.println("after properties set method called before bean is initialized and after properties set");
     }
 
-
+    @PostConstruct
     public void customInit(){
         System.out.println("Custom init called");
     }
 
+    @PreDestroy
     public void customDestroy(){
         System.out.println("Custom destroy called");
     }
