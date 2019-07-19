2587e0dff4da2fe345942a0c1af908602da60e9f
Nilakshi <nilakshi.ma1509@ict.gnu.ac.in>
2019-07-19 11:30:14 +0530

    added comments

--- src/main/java/com/stackroute/demo/Actor.java
+++ src/main/java/com/stackroute/demo/Actor.java
@@ -4,13 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
 public class Actor {
-    String name;
-    String gender;
-    int age;
+    private String name;
+    private String gender;
+    private int age;
 
+    //default constructor
     @Autowired
     Actor(){}
 
+    //setters with default values for creating beans
     @Value("cde")
     public void setName(String name) {
         this.name = name;
@@ -26,6 +28,7 @@ public class Actor {
         this.age = age;
     }
 
+    //getters
     public String getName() {
         return name;
     }
@@ -38,12 +41,14 @@ public class Actor {
         return age;
     }
 
+    //parameterised constructor
     public Actor(String name, String gender, int age) {
         this.name = name;
         this.gender = gender;
         this.age = age;
     }
 
+    //toString method
     @Override
     public String toString() {
         return "Actor{" +
